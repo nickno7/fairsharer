4e6a3a7cf54a4d0f7a47c8b2def978830fff9083
Nick Grabowski <na.grabowski@gmail.com>
2023-11-22 17:00:37 +0100

    added code in fairsharer.py

--- src/fairsharer.py
+++ src/fairsharer.py
@@ -7,12 +7,31 @@ def fair_sharer(values, num_iterations, share=0.1):
     Examples:
     fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
     fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
-
+        
     Args
     values:
         1D array of values (list or numpy array)
     num_iteration:
         Integer to set the number of iterations
     """
-    # code
+    
+    values_new = values.copy()
+    for _ in range(num_iterations):
+        # find the maximum value and its index
+        max_value = max(values_new)
+        max_index = values_new.index(max(values_new))
+
+        # calculate the left and right neighbour so it also works for the first and last index
+        left_neighbor = (max_index - 1) % len(values_new)
+        right_neighbor = (max_index + 1) % len(values_new)
+
+        # calculate the new values and update the new list
+        values_new[max_index] -= 2 * (max_value * share)
+        values_new[left_neighbor] += max_value * share
+        values_new[right_neighbor] += max_value * share
+
     return values_new
+
+print(fair_sharer([0, 1000, 800, 0], 1))
+print(fair_sharer([0, 1000, 800, 0], 2))
+print(fair_sharer([1000, 200, 600, 900],2))
